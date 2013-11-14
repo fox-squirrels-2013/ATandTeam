900988b4936fc8393a4557bdbcca48cd330f70ad
vanessa <vipyne@gmail.com>
2013-11-13 17:27:21 -0800

    adding first basic js

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  ////////////////////////////////////////////////////////////////
+  var mapOptions = {
+    center: new google.maps.LatLng(37.760130, -122.441651),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
+    mapOptions);
+////////////////////////////////////////////////////////////////
+
+///// me trying to get friggin infowindows   in right spot //////////
+  var createWindow = function(markered) {
+    return new google.maps.InfoWindow({
+      content: "this is where you clicked",
+      position: markered.position
+    });
+  }
+
+  var createMarker = function(location) {
+    return new google.maps.Marker({
+      position: location,
+      map: map
+    })
+  }
+
+  var addMarker = function(event) {
+    var markered = createMarker(event.latLng)
+    var infowindowClick = createWindow(markered)
+    infowindowClick.open(map,markered)
+  }
+
+  google.maps.event.addListener(map, 'click', addMarker);
+////////////////////////////////////////////////////////////////
 });
