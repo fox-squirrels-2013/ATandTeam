633cd91bb0b527a67787636b3bdfb4b8222f950b
vanessa <vipyne@gmail.com>
2013-11-13 17:42:57 -0800

    a map shows up. its awesome

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  ////////////////////////////////////////////////////////////////
+function initialize() {
+////////////////////////////////////////////////////////////////
   var mapOptions = {
     center: new google.maps.LatLng(37.760130, -122.441651),
     zoom: 12,
@@ -9,6 +9,22 @@ $(document).ready(function() {
   var map = new google.maps.Map(document.getElementById("map-canvas"),
     mapOptions);
 ////////////////////////////////////////////////////////////////
+
+
+/////// automatically here when page loads //////////
+  var infowindow = new google.maps.InfoWindow({
+    content: "i made something happen"
+  });
+  var marker = new google.maps.Marker({
+    position: mapOptions.center,
+    map: map,
+    title: 'boom'
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+////////////////////////////////////////////////////////////////
+
 
 ///// me trying to get friggin infowindows   in right spot //////////
   var createWindow = function(markered) {
@@ -33,4 +49,5 @@ $(document).ready(function() {
 
   google.maps.event.addListener(map, 'click', addMarker);
 ////////////////////////////////////////////////////////////////
-});
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
